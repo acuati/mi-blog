7aaf1028993f2ead7c70047008f25983bfb19e66
acuati <benalibenaliabdelkbir@gmail.com>
2024-09-25 13:27:17 +0200

    actualizo cambios definitivos

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,7 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
+  pathPrefix: `/abdelkbirbenali/patrimonionacional`,
   siteMetadata: {
     title: `El blog de Kyle`,
     author: {
